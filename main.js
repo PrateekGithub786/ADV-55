b1859425366d70fec0aecc906591b5cb7cb4499b
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-01-15 16:12:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function a() {
+	window.alert("The Taj Mahal is located on the right bank of the Yamuna River in a vast Mughal garden that encompasses nearly 17 hectares, in the Agra District in Uttar Pradesh. It was built by Mughal Emperor Shah Jahan in memory of his wife Mumtaz Mahal with construction starting in 1632 AD and completed in 1648 AD, with the mosque, the guest house and the main gateway on the south, the outer courtyard and its cloisters were added subsequently and completed in 1653 AD. The existence of several historical and Quaranic inscriptions in Arabic script have facilitated setting the chronology of Taj Mahal. For its construction, masons, stone-cutters, inlayers, carvers, painters, calligraphers, dome builders and other artisans were requisitioned from the whole of the empire and also from the Central Asia and Iran. Ustad-Ahmad Lahori was the main architect of the Taj Mahal.");
+}
+
+function a2() {
+	window.alert("Colosseum, also called Flavian Amphitheatre, giant amphitheatre built in Rome under the Flavian emperors. Construction of the Colosseum was begun sometime between 70 and 72 CE during the reign of Vespasian. It is located just east of the Palatine Hill, on the grounds of what was Nero’s Golden House. The artificial lake that was the centrepiece of that palace complex was drained, and the Colosseum was sited there, a decision that was as much symbolic as it was practical. Vespasian, whose path to the throne had relatively humble beginnings, chose to replace the tyrannical emperor’s private lake with a public amphitheatre that could host tens of thousands of Romans.");
+}
+
+function a3() {
+	window.alert("Chichen Itza was once the most revered Mayan center of worship and pilgrimage in the Yucatan Peninsula, and it is visited by more than a million tourists each year, making it the 2nd most visited archaeological zone in Mexico (just after Teotihuacan in Mexico City). The Mayan meaning for Chichen Itza is: “mouth of the well of the Itzaes” because located in this city is the Sacred Cenote, a huge sinkhole considered by the ancient inhabitants of the region one of the main entrances to the underworld. The Itzaes were known powerful mythical-historical lords, respected and revered by the Mayas back in the day.");
+}
+
+function a4() {
+	window.alert("Although the site escaped detection by the Spaniards, it may have been visited by the German adventurer Augusto Berns in 1867. However, Machu Picchu’s existence was not widely known in the West until it was “discovered” in 1911 by the Yale University professor Hiram Bingham, who was led to the site by Melchor Arteaga, a local Quechua-speaking resident. Bingham had been seeking Vilcabamba (Vilcapampa), the “lost city of the Incas,” from which the last Inca rulers led a rebellion against Spanish rule until 1572. He cited evidence from his 1912 excavations at Machu Picchu, which were sponsored by Yale University and the National Geographic Society, in his labeling of the site as Vilcabamba; however, that interpretation is no longer widely accepted. (Nevertheless, many sources still follow Bingham’s precedent and erroneously label Machu Picchu as the “lost city of the Incas.”) Evidence later associated Vilcabamba with another ruin, Espíritu Pampa, which was also discovered by Bingham. In 1964 Espíritu Pampa was extensively excavated under the direction of the American explorer Gene Savoy. The site was much deteriorated and overgrown with forest, but Savoy uncovered remains there of some 300 Inca houses and 50 or more other buildings, as well as extensive terraces, proving that Espíritu Pampa was a much larger settlement.");
+}
+
+function a5() {
+	window.alert("The statue, made of reinforced concrete clad in a mosaic of thousands of triangular soapstone tiles, sits on a square stone pedestal base about 26 feet (8 metres) high, which itself is situated on a deck atop the mountain’s summit. The statue is the largest Art Deco-style sculpture in the world.");
+}
+
+function a6() {
+	window.alert("Petra, Arabic Baṭrā, ancient city, centre of an Arab kingdom in Hellenistic and Roman times, the ruins of which are in southwest Jordan. The city was built on a terrace, pierced from east to west by the Wadi Mūsā (the Valley of Moses)—one of the places where, according to tradition, the Israelite leader Moses struck a rock and water gushed forth. The valley is enclosed by sandstone cliffs veined with shades of red and purple varying to pale yellow, and for this reason Petra was called by the 19th-century English biblical scholar John William Burgon a “rose-red city half as old as Time.” The modern town of Wadi Mūsā, situated adjacent to the ancient city, chiefly serves the steady stream of tourists who continue to visit the site.");
+}
+
+function a7() {
+	window.alert("The Great Wall developed from the disparate border fortifications and castles of individual Chinese kingdoms. For several centuries these kingdoms probably were as concerned with protection from their near neighbours as they were with the threat of barbarian invasions or raids.");
+}
